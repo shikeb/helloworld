d8172dd49595b72c4c6bd96c9a300480171d2644
shikeb <shikebali@gmail.com>
2020-08-13 04:07:09 +0500

    EDIT - add, update actions and routes updated for articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,39 @@
 class ArticlesController < ApplicationController
     
-    def index
+  def index
         @articles = Article.all()
-    end
+  end
 
-    def show
-        @article = Article.find(params[:id])
-    end
+  def show
+    @article = Article.find(params[:id])
+  end
 
-    def new
-        
-    end
+  def new
+    @article = Article.new
+	end
+	
+	def edit
+    @article = Article.find(params[:id])
+	end
 
-    def create
-        @article = Article.new(params.require(:article).permit(:title,:description))
-        @article.save
-        redirect_to article_path(@article)
-    end
+  def create
+    @article = Article.new(params.require(:article).permit(:title,:description))
+			if @article.save
+				flash[:notice] = "Article created successfully"
+        redirect_to @article
+      else
+        render 'new'
+      end
+	end
+	
+	def update
+    @article = Article.find(params[:id])
+		if @article.update(params.require(:article).permit(:title,:description))
+			flash[:notice] = "Article updated successfully"
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'articles#index'
   get 'first_test/index'
-  resources :articles , only: [:show, :index, :create, :new]
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
